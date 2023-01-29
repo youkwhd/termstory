f711f527e6f847463761c6ba7ba727c0a52491e9
youkwhd <lolywk@tutanota.com>
2023-01-29 23:52:33 +0700

    feat: display all choices

--- main.rkt
+++ main.rkt
@@ -4,7 +4,16 @@
 
 ;; (define file-story-path "")
 (define file-story (open-input-file "./__tmp_story.json"))
-
 (define json-story (read-json file-story))
-(hash-eq? json-story)
+
+(define (display-choices lst __idx)
+  (cond 
+    [(empty? lst) (newline)]
+    [else
+      (printf "(~a) ~a\n" __idx (first lst))
+      (display-choices (rest lst) (+ __idx 1))]))
+
+(displayln (hash-ref json-story 'headline))
+(newline)
+(display-choices (hash-ref json-story 'choices) 1)
 (close-input-port file-story)
